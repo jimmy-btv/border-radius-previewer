5667f21e4c07c277601cd29b324cb662857257d3
jimmy_btv <jimmy.btv22@gmail.com>
2022-11-11 13:13:00 -0800

    feat/CSS: add draggable number input & add CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 
 import React, { useRef, useState } from "react";
 interface IBorderRadius {
+  [key: string]: string;
   topLeftLeft: string;
   topLeftRight: string;
   topRightLeft: string;
@@ -13,6 +14,9 @@ interface IBorderRadius {
 }
 const Home: NextPage = () => {
   const [copySuccess, setCopySuccess] = useState<boolean>(false);
+  const [mouseStartPosition, setMouseStartPosition] = useState<number>(0);
+  const [numStart, setNumStart] = useState<number>(0);
+
   const [borderRadius, setBorderRadius] = useState<IBorderRadius>({
     topLeftLeft: "0",
     topLeftRight: "0",
@@ -24,6 +28,31 @@ const Home: NextPage = () => {
     bottomRightRight: "0",
   });
 
+  const mouseDownHandler = (e: React.MouseEvent<HTMLInputElement>): void => {
+    window.addEventListener("mousemove", mouseMoveHandler);
+    window.addEventListener("mouseup", mouseUpHandler);
+
+    setMouseStartPosition(e.pageY);
+    setNumStart(Number(borderRadius[(e.target as HTMLFormElement).name]));
+    setNumStart(isNaN(numStart) ? 0 : numStart);
+  };
+
+  const mouseMoveHandler = (e: any): void => {
+    let diff = mouseStartPosition - e.pageY;
+    let newLeft = numStart + diff;
+    newLeft = newLeft > 100 ? 100 : newLeft;
+    newLeft = newLeft < 0 ? 0 : newLeft;
+    setBorderRadius({
+      ...borderRadius,
+      [e.target.name]: newLeft,
+    });
+  };
+
+  const mouseUpHandler = (e: MouseEvent): void => {
+    window.removeEventListener("mousemove", mouseMoveHandler);
+    window.removeEventListener("mouseup", mouseUpHandler);
+  };
+
   const onChangeRadiusHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBorderRadius({
       ...borderRadius,
@@ -32,92 +61,120 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <div className="flex h-10 w-52 justify-between ">
+    <div className="flex h-screen flex-col items-center justify-center bg-[#182f55]">
+      <div className="mb-4 flex h-10 w-96 justify-between ">
         <input
           type="text"
           value={borderRadius.topLeftRight}
-          className="h-7 w-7 rounded border border-black"
+          className="z-10 h-7 w-8 cursor-ns-resize appearance-none rounded border border-black outline-none"
           name="topLeftRight"
           onChange={onChangeRadiusHandler}
+          onMouseDown={mouseDownHandler}
+          min={0}
+          max={100}
         />
         <input
           type="text"
           value={borderRadius.topRightLeft}
-          className="h-7 w-7 rounded border border-black"
+          className="h-7 w-8 cursor-ns-resize appearance-none rounded border border-black"
           name="topRightLeft"
           onChange={onChangeRadiusHandler}
+          onMouseDown={mouseDownHandler}
+          min={0}
+          max={100}
         />
       </div>
 
       <div className="flex flex-row">
-        <div className="grid flex-col content-between">
+        <div className="mr-5 grid flex-col content-between">
           <input
             type="text"
             value={borderRadius.topLeftLeft}
-            className="mr-3 h-7 w-7 rounded border border-black"
+            className="h-7 w-8 cursor-ns-resize rounded border border-black"
             name="topLeftLeft"
             onChange={onChangeRadiusHandler}
+            onMouseDown={mouseDownHandler}
+            min={0}
+            max={100}
           />
           <input
             type="text"
             value={borderRadius.bottomLeftLeft}
-            className="mr-3 h-7 w-7 rounded border border-black"
+            className=" h-7 w-8 cursor-ns-resize rounded border border-black"
             name="bottomLeftLeft"
             onChange={onChangeRadiusHandler}
+            onMouseDown={mouseDownHandler}
+            min={0}
+            max={100}
           />
         </div>
+
         <div
-          className="h-52 w-52 border border-black bg-gradient-to-r from-[#1ac929] to-[#BCED0C]"
+          className=" h-96 w-96 "
           style={{
             borderStartStartRadius: `${borderRadius.topLeftLeft}% ${borderRadius.topLeftRight}%`,
             borderStartEndRadius: `${borderRadius.topRightLeft}% ${borderRadius.topRightRight}%`,
             borderEndStartRadius: `${borderRadius.bottomLeftLeft}% ${borderRadius.bottomLeftRight}%`,
             borderEndEndRadius: `${borderRadius.bottomRightLeft}% ${borderRadius.bottomRightRight}%`,
+            background: "linear-gradient(145deg, #1a325b, #162a4d)",
+            boxShadow: `inset 18px 18px 36px #0a1322,inset -18px -18px 36px #264b88`,
           }}
         ></div>
-        <div className="ml-2 grid content-between">
+        <div className="ml-5 grid content-between">
           <input
             type="text"
             value={borderRadius.topRightRight}
-            className="h-7 w-7 rounded border border-black"
+            className="h-7 w-8 cursor-ns-resize rounded border border-black"
             name="topRightRight"
             onChange={onChangeRadiusHandler}
+            onMouseDown={mouseDownHandler}
+            min={0}
+            max={100}
           />
           <input
             type="text"
             value={borderRadius.bottomRightRight}
-            className="h-7 w-7 rounded border border-black"
+            className="h-7 w-8 cursor-ns-resize rounded border border-black"
             name="bottomRightRight"
             onChange={onChangeRadiusHandler}
+            onMouseDown={mouseDownHandler}
+            min={0}
+            max={100}
           />
         </div>
       </div>
 
-      <div className="mt-3 flex h-10 w-52 justify-between ">
+      <div className="mt-5 flex h-10 w-96 justify-between ">
         <input
           type="text"
           value={borderRadius.bottomLeftRight}
-          className="h-7 w-7 rounded border border-black"
+          className="h-7 w-8 cursor-ns-resize rounded border border-black"
           name="bottomLeftRight"
           onChange={onChangeRadiusHandler}
+          onMouseDown={mouseDownHandler}
+          min={0}
+          max={100}
         />
         <input
           type="text"
           value={borderRadius.bottomRightLeft}
-          className="h-7 w-7 rounded border border-black"
+          className="h-7 w-8 cursor-ns-resize rounded border border-black"
           name="bottomRightLeft"
           onChange={onChangeRadiusHandler}
+          onMouseDown={mouseDownHandler}
+          min={0}
+          max={100}
         />
       </div>
-      <p>
+
+      <p className="text-gray-100">
         border-radius: {borderRadius.topLeftRight}% {borderRadius.topRightLeft}%{" "}
         {borderRadius.bottomLeftRight}% {borderRadius.bottomRightLeft}% /{" "}
         {borderRadius.topLeftLeft}% {borderRadius.topRightRight}%{" "}
         {borderRadius.bottomLeftLeft}% {borderRadius.bottomRightRight}%
       </p>
       <button
-        className="m-1 border border-black bg-blue-200 p-3"
+        className="m-1 border border-black bg-gradient-to-r from-[#BCED0C] to-[#1ac929] p-3"
         onClick={() => {
           navigator.clipboard.writeText(
             `border-radius: ${borderRadius.topLeftRight}% ${borderRadius.topRightLeft}% ${borderRadius.bottomLeftRight}% ${borderRadius.bottomRightLeft}% / ${borderRadius.topLeftLeft}% ${borderRadius.topRightRight}% ${borderRadius.bottomLeftLeft}% ${borderRadius.bottomRightRight}%`
@@ -134,10 +191,23 @@ const Home: NextPage = () => {
         Copy to clipboard
       </button>
       {copySuccess && (
-        <p className="absolute translate-y-52 text-green-500">
+        <p className="bg-gradient-to-r from-[#BCED0C] to-[#1ac929] bg-clip-text text-4xl font-extrabold text-transparent">
           Copied to clipboard!
         </p>
       )}
+      <p className="mt-5 text-gray-200">
+        Made with 💚 by James Ball. Check out the repo{" "}
+        <span>
+          <a
+            href="https://github.com/jimmy-btv/border-radius-previewer"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            here
+          </a>
+        </span>
+      </p>
     </div>
   );
 };
